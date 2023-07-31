eade09d2de18464b8f41b118fb881a39f74d79c2
Script <neongamerflame@gmail.com>
2023-07-31 17:45:34 +0530

    Add files via upload

---
+++ Discord-Bot-Website (1)/pages/invite.jsx
@@ -0,0 +1,9 @@
+export default function Invite() {
+
+  return (
+
+    <meta http-equiv="refresh" content="0; URL=https://discord.com/oauth2/authorize?client_id=1032300215664914523&permissions=8&scope=applications.commands%20bot"/> 
+    
+    
+  )
+}
---
+++ Discord-Bot-Website (1)/pages/support.jsx
@@ -0,0 +1,9 @@
+export default function Support() {
+
+  return (
+
+    <meta http-equiv="refresh" content="0; URL=https://discord.gg/Z5GHQKn73z"/> 
+    
+    
+  )
+}
